f8a75443c3e771172fa47a6ddf203ecc4532bf2b
pumpkinsoup0 <112065089+pumpkinsoup0@users.noreply.github.com>
2025-04-05 22:40:48 +0900

    Add files via upload

---
+++ lecture4/4-6.py
@@ -0,0 +1,21 @@
+import skimage
+import numpy as np
+import cv2 as cv
+import time
+
+coffee = skimage.data.coffee()
+
+start = time.time()
+slic = skimage.segmentation.slic(coffee, compactness=20, n_segments=600, start_label=1)
+
+g = skimage.future.graph.cut_normalized(slic, g)
+ncut = skimage.future.graph.cut_normalized(slic, g)
+print(coffee.shape, 'Coffee 영상 분할에', time.time()-start, '초 소요')
+
+marking = skimage.segmentation.mark_boundaries(coffee, ncut)
+ncut_coffee = np.uint8(marking*255.0)
+
+cv.imshow('Normalized cut', cv.cvtColor(ncut_coffee, cv.COLOR_RGB2BGR))
+
+cv.waitKey()
+cv.destroyAllWindows()
